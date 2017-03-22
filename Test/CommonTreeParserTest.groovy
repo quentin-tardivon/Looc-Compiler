3677b1c72646aff0bb8b9c4aedcadce8562129e0
Quentin TARDIVON <quentin.tardivon@gmail.com>
2017-03-22 17:17:34 +0100

    Test3/4 and method correction

--- Test/CommonTreeParserTest.groovy
+++ Test/CommonTreeParserTest.groovy
@@ -31,6 +31,8 @@ class CommonTreeParserTest extends GroovyTestCase {
         out = treeParser.tds.get("i")
         expected = "### Variable ###\n" +  " - type       -> int\n"
         assertToString(out, expected)
+
+
     }
 
     void testConstructTDSLevel2() {
@@ -53,10 +55,127 @@ class CommonTreeParserTest extends GroovyTestCase {
         expected = "### Variable ###\n" + " - type       -> Math\n"
         assertToString(out, expected)
 
-        out = treeParser.tds.getLink("Math").getLink("pow").get("a").toString()
+        out = treeParser.tds.getLink("Math").getLink("pow").get("a")
         expected = "### Parameter ###\n" + " - type       -> int\n"
         assertToString(out, expected)
 
+        out = treeParser.tds.getLink("Math").getLink("pow").get("b")
+        expected = "### Parameter ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Math").get("pow")
+        expected = "### Method ###\n" + " - returnType -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.get("Math")
+        expected = "### Class ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Math").getLink("pow").get("i")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+
+
+    }
+
+    void testConstructTDSLevel4() {
+        String file = "./samples/Level4.looc"
+
+        ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(file))
+        LoocLexer lexer = new LoocLexer(input)
+        CommonTokenStream tokens = new CommonTokenStream(lexer)
+        LoocParser parser = new LoocParser(tokens)
+        CommonTree tree = (CommonTree)parser.program().getTree()
+
+        CommonTreeParser treeParser = new CommonTreeParser()
+        treeParser.constructTDS(tree, new SymbolTable())
+
+        String out = treeParser.tds.get("Fibonacci")
+        def expected = "### Class ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("current")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("previous")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("generation")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("init")
+        expected = "### Method ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("nextGen")
+        expected = "### Method ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").getLink("nextGen").get("temp")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("nextGenAndGet")
+        expected = "### Method ###\n" + " - returnType -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.get("f")
+        expected = "### Variable ###\n" + " - type       -> Fibonacci\n"
+        assertToString(out, expected)
+
+    }
+
+    void testConstructTDSLevel3AnonymousBlock() {
+        String file = "./samples/Level3AnonymousBlock.looc"
+
+        ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(file))
+        LoocLexer lexer = new LoocLexer(input)
+        CommonTokenStream tokens = new CommonTokenStream(lexer)
+        LoocParser parser = new LoocParser(tokens)
+        CommonTree tree = (CommonTree)parser.program().getTree()
+
+        CommonTreeParser treeParser = new CommonTreeParser()
+        treeParser.constructTDS(tree, new SymbolTable())
+
+        String out = treeParser.tds.get("Fibonacci")
+        def expected = "### Class ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("current")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("previous")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("generation")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("init")
+        expected = "### Method ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("nextGen")
+        expected = "### Method ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").getLink("nextGen").get("temp")
+        expected = "### Variable ###\n" + " - type       -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Fibonacci").get("nextGenAndGet")
+        expected = "### Method ###\n" + " - returnType -> int\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.get("f")
+        expected = "### Variable ###\n" + " - type       -> Fibonacci\n"
+        assertToString(out, expected)
     }
 
     void testConstructTDSLevel3() {
@@ -71,25 +190,47 @@ class CommonTreeParserTest extends GroovyTestCase {
         CommonTreeParser treeParser = new CommonTreeParser()
         treeParser.constructTDS(tree, new SymbolTable())
 
-        String out = treeParser.tds.getLink("Fibonacci").get("current")
-        def expected = "### Variable ###\n" +  " - type       -> int\n"
+        String out = treeParser.tds.get("Animal")
+        def expected = "### Class ###\n"
         assertToString(out, expected)
 
-        out = treeParser.tds.getLink("Fibonacci").get("previous")
-        expected = "### Variable ###\n" + " - type       -> int\n"
+        out = treeParser.tds.getLink("Animal").get("name")
+        expected = "### Variable ###\n" + " - type       -> string\n"
         assertToString(out, expected)
 
-        out = treeParser.tds.getLink("Fibonacci").get("generation")
-        expected = "### Variable ###\n" + " - type       -> int\n"
+        out = treeParser.tds.getLink("Animal").get("setName")
+        expected = "### Method ###\n"
         assertToString(out, expected)
 
-        out = treeParser.tds.getLink("Fibonacci").getLink("nextGen").get("temp")
-        expected = "### Variable ###\n" + " - type       -> int\n"
+        out = treeParser.tds.getLink("Animal").getLink("setName").get("n")
+        expected = "### Parameter ###\n" + " - type       -> string\n"
         assertToString(out, expected)
 
-        out = treeParser.tds.get("f")
-        expected = "### Variable ###\n" + " - type       -> Fibonacci\n"
+        out = treeParser.tds.get("Dog")
+        expected = "### Class ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Dog").get("whoami")
+        expected = "### Method ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.get("Cat")
+        expected = "### Class ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.getLink("Cat").get("whoami")
+        expected = "### Method ###\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.get("a1")
+        expected = "### Variable ###\n" + " - type       -> Animal\n"
+        assertToString(out, expected)
+
+        out = treeParser.tds.get("a2")
+        expected = "### Variable ###\n" + " - type       -> Animal\n"
         assertToString(out, expected)
 
     }
+
+
 }
--- src/CommonTreeParser.java
+++ src/CommonTreeParser.java
@@ -5,6 +5,7 @@ import TDS.entries.Parameter;
 import TDS.entries.Variable;
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.Tree;
+import TDS.entries.Class;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,102 +17,139 @@ public class CommonTreeParser {
 
 	protected SymbolTable tds;
 
-    protected ArrayList<String> list = new ArrayList<>();
+	protected ArrayList<String> list = new ArrayList<>();
 
-    public CommonTreeParser() {
+	public CommonTreeParser() {
 
-    }
+	}
 
-    public void parseCommonTreeParser(Tree tree) {
-	    list.add(tree.toString());
-        for (int i=0; i<tree.getChildCount(); i++) {
-            parseCommonTreeParser(tree.getChild(i));
-        }
-    }
-
-    public void constructTDS(Tree tree, SymbolTable tds) {
-	    SymbolTable newtds;
-		switch(tree.getText()) {
-			case "ROOT": this.tds = tds;
-						 for (int i=0; i<tree.getChildCount();i++) {
-						 	//System.out.println("Enter in child" + i);
-							 constructTDS(tree.getChild(i), this.tds);
-						 }
-				         break;
-
-			case "METHOD": System.out.println("Method encounter:" + tree.getChild(0).toString());
-						   newtds = new SymbolTable(tds.getImbricationLevel()+1, tds);
-						   tds.putLink(tree.getChild(0).getText(), newtds);
-						   if (tree.getChildCount() == 4) {
-						   	tds.put(tree.getChild(0).getText(), new Method(tree.getChild(2).getText()));
-						   	constructTDS(tree.getChild(1), newtds);
-						   	constructTDS(tree.getChild(3), newtds);
-						   }
-						   else if ((tree.getChildCount() == 3)) {
-							   tds.put(tree.getChild(0).getText(), new Method(tree.getChild(1).getText()));
-							   constructTDS(tree.getChild(2), newtds);
-						   }
-						   else {
-							   tds.put(tree.getChild(0).getText(), new Method());
-							   constructTDS(tree.getChild(1), newtds);
-						   }
-
-				           break;
-
-			case "FORMAL_PARAMS": System.out.println("Formal parameters encounter:");
-									for(int i=0; i<tree.getChildCount(); i++ ) {
-										constructTDS(tree.getChild(i), tds);
-									}
-									break;
-
-			case "FORMAL_PARAM": System.out.println("Formal parameters encounter:");
-									 tds.put(tree.getChild(0).getText(), new Parameter(tree.getChild(1).getText()));
-
-			case "VAR_DEC": System.out.println("Var encounter:" + tree.getChild(0).toString());
-						    tds.put(tree.getChild(0).getText(), new Variable(tree.getChild(1).getText()));
-						    break;
-
-			case "CLASS_DEC": System.out.println("Class encounter:" + tree.getChild(0).toString());
-							  newtds = new SymbolTable(tds.getImbricationLevel()+1, tds);
-							  tds.putLink(tree.getChild(0).getText(), newtds);
-							  for (int j = 1; j<tree.getChildCount();j++) {
-							  	constructTDS(tree.getChild(j), newtds);
-							  }
-
-							  break;
-
-
-			case "BLOCK": this.tds.putLink(tree.getChild(0).getText(), new SymbolTable(this.tds.getImbricationLevel()+1, this.tds));
-				          break;
-
-			case "THEN": this.tds.putLink(tree.getChild(0).getText(), new SymbolTable(this.tds.getImbricationLevel()+1, this.tds)); //Certainly not working
-				       break;
-
-			case "ELSE": this.tds.putLink(tree.getChild(0).getText(), new SymbolTable(this.tds.getImbricationLevel()+1, this.tds)); //Certainly not working
-						 break;
-
-			case "VARS": for(int i= 0; i<tree.getChildCount();i++) {
-							constructTDS(tree.getChild(i), tds);
-						 }
-						 break;
-
-			case "METHODS": for(int i = 0; i<tree.getChildCount(); i++) {
-								constructTDS(tree.getChild(i), tds);
-							}
-							break;
-
-			case "BODY": for (int i = 0; i<tree.getChildCount(); i++) {
-					        constructTDS(tree.getChild(i), tds);
-						 }
-						 break;
-
-			default: for (int i=0; i<tree.getChildCount();i++) {
-				System.out.println("## Default case " + tree);
-				constructTDS(tree.getChild(i), tds);
-			}
-			break;
+	public void parseCommonTreeParser(Tree tree) {
+		list.add(tree.toString());
+		for (int i = 0; i < tree.getChildCount(); i++) {
+			parseCommonTreeParser(tree.getChild(i));
 		}
-    }
+	}
+
+	public void constructTDS(Tree tree, SymbolTable tds) {
+		SymbolTable newtds;
+		switch (tree.getText()) {
+			case "ROOT":
+				this.tds = tds;
+				for (int i = 0; i < tree.getChildCount(); i++) {
+					//System.out.println("Enter in child" + i);
+					constructTDS(tree.getChild(i), this.tds);
+				}
+				break;
+
+			case "METHOD":
+				System.out.println("Method encounter:" + tree.getChild(0).toString());
+				newtds = new SymbolTable(tds.getImbricationLevel() + 1, tds);
+				tds.putLink(tree.getChild(0).getText(), newtds);
+				if (tree.getChildCount() == 4) {
+					tds.put(tree.getChild(0).getText(), new Method(tree.getChild(2).getText()));
+					constructTDS(tree.getChild(1), newtds);
+					constructTDS(tree.getChild(3), newtds);
+				} else if ((tree.getChildCount() == 3) && tree.getChild(1).getText() == "FORMAL_PARAMS") {
+					tds.put(tree.getChild(0).getText(), new Method());
+					constructTDS(tree.getChild(1), newtds);
+					constructTDS(tree.getChild(2), newtds);
+				} else if ((tree.getChildCount() == 3)) {
+					tds.put(tree.getChild(0).getText(), new Method(tree.getChild(1).getText()));
+					constructTDS(tree.getChild(2), newtds);
+				} else {
+					tds.put(tree.getChild(0).getText(), new Method());
+					constructTDS(tree.getChild(1), newtds);
+				}
+
+				break;
+
+			case "FORMAL_PARAMS":
+				System.out.println("Formal parameters encounter:");
+				for (int i = 0; i < tree.getChildCount(); i++) {
+					constructTDS(tree.getChild(i), tds);
+				}
+				break;
+
+			case "FORMAL_PARAM":
+				System.out.println("Formal parameters encounter:");
+				tds.put(tree.getChild(0).getText(), new Parameter(tree.getChild(1).getText()));
+				break;
+
+			case "VAR_DEC":
+				System.out.println("Var encounter:" + tree.getChild(0).toString());
+				tds.put(tree.getChild(0).getText(), new Variable(tree.getChild(1).getText()));
+				break;
+
+			case "CLASS_DEC":
+				System.out.println("Class encounter:" + tree.getChild(0).toString());
+				tds.put(tree.getChild(0).getText(), new Class(tree.getChild(0).getText()));
+				newtds = new SymbolTable(tds.getImbricationLevel() + 1, tds);
+				tds.putLink(tree.getChild(0).getText(), newtds);
+				for (int j = 1; j < tree.getChildCount(); j++) {
+					constructTDS(tree.getChild(j), newtds);
+				}
+
+				break;
+
+
+			case "BLOCK":
+				newtds = new SymbolTable(tds.getImbricationLevel() + 1, tds); //We have to generate a nam
+				tds.putLink(tree.getChild(0).getText(), newtds);
+				for (int j = 1; j < tree.getChildCount(); j++) {
+					constructTDS(tree.getChild(j), newtds);
+				}
+				break;
+
+			case "IF":
+				newtds = new SymbolTable(tds.getImbricationLevel() + 1, tds); //We have to generate a nam
+				tds.putLink(tree.getChild(0).getText(), newtds);
+				for (int j = 1; j < tree.getChildCount(); j++) {
+					constructTDS(tree.getChild(j), newtds);
+				}
+				break;
+
+			case "THEN":
+				newtds = new SymbolTable(tds.getImbricationLevel() + 1, tds); //We have to generate a name
+				tds.putLink(tree.getChild(0).getText(), newtds);
+				for (int j = 1; j < tree.getChildCount(); j++) {
+					constructTDS(tree.getChild(j), newtds);
+				}
+				break;
+
+			case "ELSE":
+				newtds = new SymbolTable(tds.getImbricationLevel() + 1, tds); //We have to generate a nam
+				tds.putLink(tree.getChild(0).getText(), newtds);
+				for (int j = 1; j < tree.getChildCount(); j++) {
+					constructTDS(tree.getChild(j), newtds);
+				}
+				break;
+
+			case "VARS":
+				for (int i = 0; i < tree.getChildCount(); i++) {
+					constructTDS(tree.getChild(i), tds);
+				}
+				break;
+
+			case "METHODS":
+				for (int i = 0; i < tree.getChildCount(); i++) {
+					constructTDS(tree.getChild(i), tds);
+				}
+				break;
+
+			case "BODY":
+				for (int i = 0; i < tree.getChildCount(); i++) {
+					constructTDS(tree.getChild(i), tds);
+				}
+				break;
+
+			default:
+				for (int i = 0; i < tree.getChildCount(); i++) {
+					System.out.println("## Default case " + tree);
+					constructTDS(tree.getChild(i), tds);
+				}
+				break;
+		}
+	}
 
 
 }
