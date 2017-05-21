95785c52fab5e781657e1bf45e075aa7f6a89ff3
Quentin TARDIVON <quentin.tardivon@gmail.com>
2017-05-21 12:05:59 +0200

    Add test anonymous

--- Test/AssemblerTest.groovy
+++ Test/AssemblerTest.groovy
@@ -40,6 +40,10 @@ class AssemblerTest extends GroovyTestCase {
         compileAndGenerate("Level4.looc", "+50", "+100")
     }
 
+    void testASmLevel5() {
+        compileAndGenerate("Level5.looc", "+12","+50", "+100")
+    }
+
     void testASmLevelRW() {
         compileAndGenerate("LevelRW.looc", "i+j :", "+2878","i (i:=j-i) :", "-3788", "x+i/100 :", "-36","helloworld")
     }
